a84dbfedb2febd8157230e8a77c1ce9c4207cf06
bui1@ualberta.ca <bui1@ualberta.ca>
2021-02-01 18:07:45 -0700

    refactor to use generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,6 +7,25 @@ from django.shortcuts import get_object_or_404, render
 # Create your views here.
 
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
